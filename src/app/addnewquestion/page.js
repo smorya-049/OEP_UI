ef5e88267fc697b94735bf581925b424cae896dc
Surbhi-2908 <yadavs0308@gmail.com>
2024-07-27 14:30:14 +0530

    Testnav

---
+++ src/app/addnewquestion/page.js
@@ -0,0 +1,15 @@
+// pages/index.js
+"use client";
+import Header from './components/Testnav.js';
+import QuestionForm from './components/Questionform.js';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <QuestionForm />
+        </div>
+    );
+};
+
+export default Home;
