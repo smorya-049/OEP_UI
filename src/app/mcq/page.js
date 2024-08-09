b7bcdcbf5c6df7f49efb0290e6d27096d98b6d90
sanyadixit <dixitsanya9@gmail.com>
2024-08-09 21:17:24 +0530

    mcq

--- src/app/mcq/page.js
+++ src/app/mcq/page.js
@@ -7,16 +7,17 @@ export default function mcq(){
             <div><QuesNav/>
             </div>
             <div className="float-right pt-7 text-4xl mr-20 text-red-600 font-semibold">00:00</div>
-            <div className="ml-64 pt-20"><h1>QUESTION:1 (Multiple Choice Question)</h1><br>
+            <div className="ml-56 pt-20"><h1>QUESTION:1 (Multiple Choice Question)</h1><br>
             </br><br></br>
             <form>
             <b>Distinguish between Stock and Flow. </b><br></br><br></br>
-            <input type="radio" value="a" name="answer"/>  Option-1<br></br>
-            <input type="radio" value="b" name="answer"/>  Option-2<br></br>
-            <input type="radio" value="c" name="answer"/>  Option-3<br></br>
-            <input type="radio" value="d" name="answer"/>  Option-4<br></br>
+            <input type="radio" value="a" name="answer"/> Option-1<br></br>
+            <input type="radio" value="b" name="answer"/> Option-2<br></br>
+            <input type="radio" value="c" name="answer"/> Option-3<br></br>
+            <input type="radio" value="d" name="answer"/> Option-4<br></br>
             <br></br><br></br><br></br>
             <div><button className={styles.back}>Back</button>
+            <button className={styles.save}>Save</button>
             <button className={styles.next}>Next</button></div>
             </form></div>
         </div>
